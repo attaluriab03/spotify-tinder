b0b7439422f0a1e1af014aade097064c4e737bcb
Abhinav Attaluri <attaluriabhi@gmail.com>
2024-09-29 02:00:14 -0400

    setup backend with firebase

---
+++ spotify-tinder/backend/app.js
@@ -0,0 +1,358 @@
+var express = require("express");
+var request = require("request");
+var crypto = require("crypto");
+var cors = require("cors");
+var querystring = require("querystring");
+var cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+var client_id = process.env.CLIENT_ID; // your clientId
+var client_secret = process.env.CLIENT_SECRET; // Your secret
+var redirect_uri = "http://localhost:3000/callback"; // Your redirect uri -> port = 3000
+
+const db = require("./firebase");
+const {
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+  getDoc,
+  query,
+  where,
+} = require("firebase/firestore");
+
+const generateRandomString = (length) => {
+  return crypto.randomBytes(60).toString("hex").slice(0, length);
+};
+
+var stateKey = "spotify_auth_state";
+
+var app = express();
+
+app
+  .use(express.static(__dirname + "/public"))
+  .use(cors())
+  .use(cookieParser());
+
+app.get("/login", function (req, res) {
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope =
+    "user-read-private user-read-email user-top-read user-follow-read user-library-read";
+  res.redirect(
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state,
+        show_dialog: true, // to make sure you always go to spotify log-in everytime log in button is clicked
+      })
+  );
+});
+
+
+
+app.get("/callback", function (req, res) {
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect(
+      "/#" +
+        querystring.stringify({
+          error: "state_mismatch",
+        })
+    );
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code",
+      },
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " +
+          new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+      },
+      json: true,
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        var options = {
+          url: "https://api.spotify.com/v1/me",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true,
+        };
+
+        var user_id = null;
+
+        // use the access token to access the Spotify Web API
+        request.get(options, async function (error, response, body) {
+          // adding info from JSON returned when you log in
+          var user_name = body.display_name;
+          user_id = body.id;
+
+          // we can also pass the token to the browser to make requests from there
+          res.redirect(
+            "http://localhost:5173/#" +
+              querystring.stringify({
+                access_token: access_token,
+                refresh_token: refresh_token,
+                user_id: user_id,
+                user_name: user_name,
+              })
+          );
+
+          // add user info to firebase
+          try {
+            const usersRef = collection(db, "users");
+            const q = query(usersRef, where("userid", "==", user_id));
+            const querySnapshot = await getDocs(q);
+
+            if(querySnapshot.empty){
+              await addDoc(collection(db, "users"), {
+                public: true,
+                username: body.display_name,
+                profilepic: body.images[1] ? body.images[1].url : null,
+                followercount: body.followers.total,
+                userid: body.id,
+              });
+            }            
+          } catch (e) {
+            console.error("Error adding user: ", e);
+          }
+        });
+
+        // Fetch top 6 month artists
+        var topArtistsHalfYear = {
+          url: "https://api.spotify.com/v1/me/top/artists?time_range=medium_term&limit=20&offset=0",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true,
+        };
+
+        request.get(topArtistsHalfYear, async function (error, response, body) {
+          if (error) {
+            console.error("Error:", error);
+            return;
+          }
+
+          const usersRef = collection(db, "users");
+          const q = query(usersRef, where("userid", "==", user_id));
+
+          try {
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+              const userToUpdate = querySnapshot.docs[0].id;
+
+              var allTopArtistInfo = body.items;
+              var artistInfoToPush = [];
+              allTopArtistInfo.map((artist) =>
+                artistInfoToPush.push({
+                  artistimage: artist.images[0].url,
+                  artistname: artist.name,
+                  artistid: artist.id,
+                })
+              );
+
+              updateDoc(doc(db, "users", userToUpdate), {
+                topArtistHalfYear: artistInfoToPush,
+              });
+            }
+          } catch (e) {
+            console.error("Error getting top artist 6 month: ", e);
+          }
+        });
+
+        // Fetch top months songs
+        var topSongMonth = {
+          url: "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=20&offset=0",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true,
+        };
+
+        request.get(topSongMonth, async function (error, response, body) {
+          if (error) {
+            console.error("Error:", error);
+            return;
+          }
+
+          const usersRef = collection(db, "users");
+          const q = query(usersRef, where("userid", "==", user_id));
+
+          try {
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+              const userToUpdate = querySnapshot.docs[0].id;
+
+              var allTopSongsInfo = body.items;
+              var songInfoToPush = [];
+              allTopSongsInfo.map((song) =>
+                songInfoToPush.push({
+                  albumimage: song.album.images[0].url,
+                  albumname: song.album.name,
+                  songname: song.name,
+                  artistname: song.artists,
+                  songid: song.id,
+                })
+              );
+
+              updateDoc(doc(db, "users", userToUpdate), {
+                topSongMonth: songInfoToPush,
+              });
+            }
+          } catch (e) {
+            console.error("Error getting top song of the month: ", e);
+          }
+        });
+
+        // Fetch saved albums
+        var allAlbums = {
+          url: "https://api.spotify.com/v1/me/albums?limit=25&offset=0",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true,
+        };
+
+        request.get(allAlbums, async function (error, response, body) {
+          if (error) {
+            console.error("Error for all albums:", error);
+            return;
+          }
+
+          const usersRef = collection(db, "users");
+          const q = query(usersRef, where("userid", "==", user_id));
+
+          try {
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+              const userToUpdate = querySnapshot.docs[0].id;
+
+              var allAlbums = body.items;
+              var albumInfoToPush = [];
+              allAlbums.map((album) =>
+                albumInfoToPush.push({
+                  albumname: album.album.name,
+                  albumimage: album.album.images[0].url,
+                  artistname: album.album.artists,
+                  albumid: album.album.id,
+                })
+              );
+
+              updateDoc(doc(db, "users", userToUpdate), {
+                savedalbums: albumInfoToPush,
+              });
+            }
+          } catch (e) {
+            console.error("Error getting saved albums: ", e);
+          }
+        });
+
+        // Fetch saved songs
+        var allSongs = {
+          url: "https://api.spotify.com/v1/me/tracks?limit=30",
+          headers: { Authorization: "Bearer " + access_token },
+          json: true,
+        };
+
+        request.get(allSongs, async function (error, response, body) {
+          if (error) {
+            console.error("Error for all songs:", error);
+            return;
+          }
+
+          const usersRef = collection(db, "users");
+          const q = query(usersRef, where("userid", "==", user_id));
+
+          try {
+            const querySnapshot = await getDocs(q);
+            if (!querySnapshot.empty) {
+              const userToUpdate = querySnapshot.docs[0].id;
+
+              var allSongsInfo = body.items;
+              var allSongsInfoToPush = [];
+              allSongsInfo.map((song) =>
+                allSongsInfoToPush.push({
+                  albumimage: song.track.album.images[0].url,
+                  albumname: song.track.album.name,
+                  artistname: song.track.artists,
+                  songname: song.track.name,
+                  songid: song.track.id,
+                })
+              );
+
+              updateDoc(doc(db, "users", userToUpdate), {
+                allsongs: allSongsInfoToPush,
+              });
+            }
+          } catch (e) {
+            console.error("Error getting all songs info: ", e);
+          }
+        });
+      } else {
+        res.redirect(
+          "http://localhost:5173/#" +
+            querystring.stringify({
+              error: "invalid_token",
+            })
+        );
+      }
+    });
+  }
+});
+
+app.get("/refresh_token", function (req, res) {
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: "https://accounts.spotify.com/api/token",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      Authorization:
+        "Basic " +
+        new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+    },
+    form: {
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token,
+        refresh_token = body.refresh_token;
+      res.send({
+        access_token: access_token,
+        refresh_token: refresh_token,
+      });
+    }
+  });
+});
+
+/* --------------------------------- EXPRESS ROUTES ------------------------------------- */
+app.use(express.json());
+
+const userRouter = require("./users");
+
+app.use("/users", userRouter);
+/* ---------------------------------------------------------------------------------------- */
+
+console.log("Listening on 3000");
+app.listen(3000); // port -> 3000
---
+++ spotify-tinder/backend/users.js
@@ -0,0 +1,87 @@
+var request = require("request");
+const express = require("express");
+const db = require("./firebase");
+const {
+  collection,
+  getDocs,
+  updateDoc,
+  doc,
+  addDoc,
+  deleteDoc,
+  getDoc,
+  query,
+  where,
+} = require("firebase/firestore");
+const router = express.Router();
+
+
+router.get("/", async (req, res) => {
+  try {
+    let ret = [];
+    const docRef = await getDocs(collection(db, "users"));
+
+    docRef.forEach((doc) => {
+      ret.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    res.status(200).json(ret);
+  } catch (e) {
+    res.status(400).json({ error: `Error fetching user data ${e}` });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+
+    const docRef = doc(db, "users", req.params.id);
+    const docSnap = await getDoc(docRef);
+   
+    
+    if (docSnap.exists()) {
+      res.status(200).json(docSnap.data());
+    } else {
+      console.log("Document from user does not exist");
+    }
+  } catch (e) {
+    res.status(400).json({ error: `Error fetching user data ${e}` });
+  }
+})
+
+//Get Doc ID
+router.put("/query/:query", async (req, res) => {
+  try {
+    const userId = req.body.userId;
+    console.log(userId);
+    const q = query(collection(db,"users"), where("userid", "==", userId));
+    const querySnapshot = await getDocs(q);
+    console.log(querySnapshot.docs[0].id);
+    if (querySnapshot) {
+      res.status(200).json(querySnapshot.docs[0].id);
+    } else {
+      console.log("Document does not exist");
+    }
+  } catch (e) {
+    res.status(400).json({error: `Error fetching user data ${e}`})
+  }
+})
+
+//update user privacy setting
+router.put("/public/:id", async (req, res) => {
+  try {
+      const isPublic = req.body.public;
+      const userId = req.body.userId;
+
+      await updateDoc(doc(db, "users", userId), {
+          public: isPublic,
+      });
+      res.status(200).json({ message: "Profile privacy updated successfully" });
+  } catch (e) {
+      res.status(400).json({ error: e.message });
+  }
+});
+
+
+module.exports = router;
